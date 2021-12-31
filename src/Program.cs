4769669388031e35307f343d2aab5f67d8e33c7d
dmitriy.b <dmitriy.b@smplt.net>
2021-12-31 10:41:14 +0500

    Use serilog & migrations on start

--- src/Program.cs
+++ src/Program.cs
@@ -2,45 +2,56 @@ using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using EducationPromo.Postgres;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 namespace EducationPromo
 {
 	public class Program
 	{
-		private const string ConnectionstringParamName = "CONNECTIONSTRING";
+		private const string ConnectionStringVariable = "CONNECTIONSTRING";
 
 		public static void Main(string[] args)
 		{
+			Console.Title = "MyJetEducation EducationPromo";
+
+			string connectionString = Environment.GetEnvironmentVariable(ConnectionStringVariable);
+			if (string.IsNullOrWhiteSpace(connectionString))
+			{
+				ShowError($"Error! Environment variable \"{ConnectionStringVariable}\" not set.");
+				return;
+			}
+
 			WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-			builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
-
-			string postgresConnectionString = Environment.GetEnvironmentVariable(ConnectionstringParamName);
-			if (string.IsNullOrWhiteSpace(postgresConnectionString))
-				throw new ArgumentException($"Error! Environment variable \"{ConnectionstringParamName}\" not set.");
-
-			builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
-				containerBuilder.Register(_ =>
-				{
-					var databaseContext = new DatabaseContext(new DbContextOptionsBuilder<DatabaseContext>().UseNpgsql(postgresConnectionString).Options);
-					databaseContext.Database.Migrate();
-					return databaseContext;
-				}));
+			builder.Host
+				.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+				.UseSerilog((_, configuration) => configuration.WriteTo.Console())
+				.ConfigureContainer<ContainerBuilder>(containerBuilder => containerBuilder.Register(_ =>
+					new DatabaseContext(new DbContextOptionsBuilder<DatabaseContext>().UseNpgsql(connectionString).Options)));
 
 			builder.Services.AddRazorPages();
 
 			WebApplication app = builder.Build();
 
-			if (!app.Environment.IsDevelopment())
-				app.UseExceptionHandler("/Error");
+			app.Services.GetService<DatabaseContext>()?.Database.Migrate();
 
 			app.UseDefaultFiles();
 			app.UseStaticFiles();
-			app.UseRouting();
 			app.MapControllers();
-			app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+			app.UseRouting();
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapRazorPages();
+				endpoints.MapControllers();
+			});
 
 			app.Run();
+		}
+
+		private static void ShowError(string message)
+		{
+			Console.WriteLine(message);
+			throw new Exception(message);
 		}
 	}
 }
